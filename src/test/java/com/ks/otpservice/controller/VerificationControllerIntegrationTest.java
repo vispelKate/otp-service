0a49eddbea6e651e3f84ec1e6fa8b11530c92aba
vispelKate <kateskacheck@gmail.com>
2018-10-14 15:55:58 +0300

    Otp tests

---
+++ src/test/java/com/ks/otpservice/controller/VerificationControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.ks.otpservice.controller;
+
+import com.ks.otpservice.utils.VerificationStatusEnum;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerificationControllerIntegrationTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setUp()  {
+        this.mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void verifyCode() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/verify/otp"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(VerificationStatusEnum.VERIFIED.name()))
+                .andDo(print());
+    }
+}
