cf1121dff92b86d8f54ff518fa9e054ea74c601b
vispelKate <kateskacheck@gmail.com>
2018-10-17 01:53:14 +0300

    controller

---
+++ src/main/java/com/ks/otpservice/controller/OtpCodeController.java
@@ -0,0 +1,40 @@
+package com.ks.otpservice.controller;
+
+import com.ks.otpservice.service.OtpCodeService;
+import com.ks.otpservice.utils.StatusEnum;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OtpCodeController {
+
+    @Autowired
+    OtpCodeService otpCodeService;
+
+    @PostMapping("/send/otp")
+    @ApiOperation(value="send new created otp code ")
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="OK"),
+            @ApiResponse(code=500,message="Internal Server Error"),
+            @ApiResponse(code=404,message="ClientServiceId not found")
+    })
+    public ResponseEntity sendOtpCode(){
+        return ResponseEntity.ok(StatusEnum.CODE_SENT.name());
+    }
+
+    @PostMapping("/verify/otp")
+    @ApiOperation(value="verify otp code ")
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="OK"),
+            @ApiResponse(code=500,message="Internal Server Error"),
+            @ApiResponse(code=404,message="ClientServiceId not found")
+    })
+    public ResponseEntity verifyOtpCode(){
+        return ResponseEntity.ok(StatusEnum.VERIFIED.name());
+    }
+}
