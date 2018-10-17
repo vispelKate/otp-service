583083a620edf0404ad557e9d465675a7e00f8e0
vispelKate <kateskacheck@gmail.com>
2018-10-17 13:31:34 +0300

    deleted wrong repositories

--- src/main/java/repository/ServiceCleintRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import com.ks.otpservice.model.ServiceClient;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceCleintRepository extends CrudRepository<ServiceClient, Long> {
-}
